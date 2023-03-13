94fac6570252b7cb5292e78b849c05b932bc453a
Rajazz <aunurja@gmail.com>
2023-03-13 10:37:52 +0700

    senin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,9 +16,8 @@ class DashboardController extends Controller
     public function buy (){
         $tiket = Ticket::join('category_ticket', 'category_ticket.id', '=', 'ticket.id_category')
         ->join('users','users.id','=','ticket.officer')
-        ->join('booking','booking.id_ticket','=','ticket.id')
         ->get(['category_ticket.category', 'ticket.name', 'ticket.date', 'ticket.id','ticket.quota','ticket.price'
-        ,'users.name','booking.status']);
+        ,'users.name',]);
         return view('ticket/buyticket',compact('tiket'),['title'=>'buy']);
     }
     public function add (){
--- resources/views/ticket/buyticket.blade.php
+++ resources/views/ticket/buyticket.blade.php
@@ -14,7 +14,7 @@
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>
 @endif
-      
+
 
   <div class="table-responsive mt-4">
     <table class="table table-striped table-sm table-bordered">
